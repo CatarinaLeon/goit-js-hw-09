3614aca3605fe339f85e2aa08a035099ff27a782
Kateryna Vasylieva <catarina.leon.203@gmail.com>
2021-11-18 23:07:06 +0200

    dz

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startButton.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+
+    }, 1000);
+    startButton.setAttribute('disabled', true);
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    startButton.removeAttribute('disabled');
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,31 @@
+import Notiflix from "../../node_modules/notiflix";
+
+const query = (selector) => document.querySelector(selector);
+const form = query(".form")
+
+
 function createPromise(position, delay) {
+  setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+},delay)
 }
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {elements: { delay, step, amount}} = event.currentTarget;
+ for(let i =0; i< amount.value; i++){
+ let repet = i+1
+ let del = Number(delay.value)+Number(i*step.value);
+  createPromise(repet,del)
+}
+
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", handleSubmit);
